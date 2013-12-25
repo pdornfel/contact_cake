94a2e0516721e5249eeadac041974fef88d08ef6
Paul Dornfeld <pdornfel@gmail.com>
2013-12-25 09:37:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactCake::Application.config.secret_key_base = 'db79afcd5aba047d8737d9d731d0fe2bd026a157047d8a4abcb52a1ae8174c19a48d2db865c7ae15e2911fb886aa0a0da7e402a30db30abf86ebb29685023b76'

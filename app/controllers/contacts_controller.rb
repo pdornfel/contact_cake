811d105f8c1193a7d2ff85c77daa32271f4797d8
Paul Dornfeld <pdornfel@gmail.com>
2013-12-29 18:37:21 -0500

    added views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,10 +13,20 @@ class ContactsController < ApplicationController
     redirect_to :contacts
   end
 
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
   private
 
   def contact_params
     params.require(:contact).permit(:name, :user_id)
   end
+
+
 
 end

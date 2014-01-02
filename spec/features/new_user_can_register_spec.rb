c7f8724cb2c59fb1c1866914f51c6ca1936d94e4
Paul Dornfeld <pdornfel@gmail.com>
2014-01-02 15:40:01 -0500

    green specs

--- spec/features/new_user_can_register_spec.rb
+++ spec/features/new_user_can_register_spec.rb
@@ -20,9 +20,23 @@ feature 'a new user can sign up =>' do
     expect(page).to_not have_content("Sign In")
   end
 
-  scenario "a new user DOES NOT supply required information"
+  scenario "a new user DOES NOT supply required information" do
+    visit new_user_registration_path
+    click_button "Sign Up"
+    expect(page).to have_content("Please review the problems below:")
+  end
 
-  scenario "password confirmation does not match"
+  scenario "password confirmation does not match" do
+    visit new_user_registration_path
+    fill_in "First Name", with: "Paul"
+    fill_in "Last Name", with: "Dornfeld"
+    fill_in "Email", with: "pdorfel@gmail.com"
+    fill_in "user_password", with: "123456789"
+    fill_in "Password Confirmation", with: "password"
+    click_button "Sign Up"
+    expect(page).to have_content("doesn't match Password")
+
+  end
 
 
 end

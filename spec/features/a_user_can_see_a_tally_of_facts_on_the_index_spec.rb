61f41e2ed3dcd47847f8339693420c138db16f98
Paul Dornfeld <pdornfel@gmail.com>
2014-01-04 20:40:33 -0500

    added tally

--- spec/features/a_user_can_see_a_tally_of_facts_on_the_index_spec.rb
+++ spec/features/a_user_can_see_a_tally_of_facts_on_the_index_spec.rb
@@ -2,6 +2,25 @@ require 'spec_helper'
 
 feature "A user can see a count of facts for a contact on the index page" do
 
-  scenario "A user is on the index page and wants to see a number aount of facts for a contact"
+  let(:user) { FactoryGirl.create(:user) }
+
+  scenario "A user is on the index page and wants to see a number aount of facts for a contact" do
+      contact = FactoryGirl.create(:contact)
+      visit root_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_button "Sign in"
+      visit new_contact_path
+      fill_in "Name", with: contact.name
+      click_button "Create Contact"
+      visit contact_path(contact)
+      click_link "Add Fact"
+      fill_in "Fact", with: "OMG love this person"
+      click_button "Add Fact"
+      expect(page).to have_content "'OMG love this person' added successfully"
+      expect(page).to have_content "OMG love this person"
+      visit root_path
+      expect(page).to have_content "1"
+  end
 
 end

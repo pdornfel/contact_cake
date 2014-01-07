8c16e199a1b70919c006d2a0b91fc3ee9c43b53a
Paul Dornfeld <pdornfel@gmail.com>
2014-01-06 20:54:36 -0500

    removed mailer method

--- app/controllers/user_inquiries_controller.rb
+++ app/controllers/user_inquiries_controller.rb
@@ -9,7 +9,7 @@ class UserInquiriesController < ApplicationController
 
     if @user_inquiry.submit
       flash['success'] = "Your message has been sent"
-      UserMailer.send_message(@user_inquiry)
+      # UserMailer.send_message(@user_inquiry)
       if user_signed_in?
         redirect_to root_path
       else

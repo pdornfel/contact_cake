5dbb803ea4d8edf0387226797595779ef613a372
Paul Dornfeld <pdornfel@gmail.com>
2014-01-07 19:53:09 -0500

    added file

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,5 +1,3 @@
-require 'mail'
-
 if Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
@@ -7,16 +5,4 @@ if Rails.env.development?
     port: 1025
   }
 
-end
-
-Mail.defaults do
-  delivery_method :smtp, {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
 end

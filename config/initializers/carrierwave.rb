c55679f31d2aef80eb04f8e4bdfbfdd5ac61a260
Paul Dornfeld <pdornfel@gmail.com>
2014-01-09 12:08:57 -0500

    fixed secret keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['aws_access_key'],                        # required
-    :aws_secret_access_key  => ENV['aws_secret_key']                        # required
+    :aws_access_key_id      => ENV['aws_access_key_id'],                        # required
+    :aws_secret_access_key  => ENV['aws_secret_access_key']                        # required
   }
   config.fog_directory  = "contact-cake-#{Rails.env}"                     # required
 end

d67135e2d00880acd5f2f14de8f52b38d971f7b9
Paul Dornfeld <pdornfel@gmail.com>
2014-02-05 11:02:32 -0500

    sidekiq troubleshooting

--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -4,7 +4,7 @@ class EmailWorker
   def perform(contact_id)
     contact = Contact.find_by(id: contact_id)
     puts "hello contact: #{contact.name}"
-    FollowUpMailer.delay_for(3.days).send_follow_up(contact)
+    FollowUpMailer.delay_for(3.days).send_follow_up(contact).deliver
   end
 
 end
